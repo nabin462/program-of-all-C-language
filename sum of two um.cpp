5ea46047c06247e157b4cf86ceafabee1081482a
nabin <budhathokinabin700@gmail.com>
2026-07-06 18:23:30 +0545

    add sum of two number file

---
+++ sum of two um.cpp
@@ -0,0 +1,11 @@
+//sum of two number
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,s;
+    cout<<"enter two number"<<endl;
+    cin>>a>>b;
+    s=a+b;
+    cout<<"sum="<<s;
+    return 0;
+}
