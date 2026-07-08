669381a056cc18c7c3072849225b9f3e6fc64bb5
nabin <budhathokinabin700@gmail.com>
2026-07-08 18:00:38 +0545

    .\area of rectangle.cpp

---
+++ area of rectangle.cpp
@@ -0,0 +1,11 @@
+//program to find the area of rectangle
+#include<iostream>
+using namespace std;
+int main(){
+    int l,b,a;
+    cout<<"enter two number"<<endl;
+    cin>>l>>b;
+    a=l*b;
+    cout<<"area="<<a<<endl;
+    return 0;
+}
