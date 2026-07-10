7c9d3d1e25edfda70005e05b801d4f51e054eb39
nabin <budhathokinabin700@gmail.com>
2026-07-10 15:58:29 +0545

    add area

---
+++ area .cpp
@@ -0,0 +1,11 @@
+//program to find area of rectangle
+#include<iostream>
+using namespace std;
+int main(){
+    int l,b,a;
+    cout<<"enter length and breadth"<<endl;
+    cin>>l>>b;
+    a=l*b;
+    cout<<"Area="<<a<<endl;
+    return 0;
+}
