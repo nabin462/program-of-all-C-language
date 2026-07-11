a71742c5c6a5791ce89dbeb96df78395c2a42742
nabin <budhathokinabin700@gmail.com>
2026-07-11 13:26:02 +0545

    add check num positive or negative file

---
+++ check num positive or negative.cpp
@@ -0,0 +1,15 @@
+// Program to check the number is positive or negative
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Eneter the number"<<endl;
+    cin>>a;
+    if(a>0){
+        cout<<"the number is positive";
+    }
+    else{
+        cout<<"the number is negative";
+    }
+    return 0;
+}
