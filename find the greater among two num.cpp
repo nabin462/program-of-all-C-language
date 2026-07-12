b9dcca94b363d8f1563b06d3dae198f09b85c50c
nabin <budhathokinabin700@gmail.com>
2026-07-12 10:21:13 +0545

    add greater among two num

---
+++ find the greater among two num.cpp
@@ -0,0 +1,15 @@
+//Program to find the greater between two number
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two number"<<endl;
+    cin>>a>>b;
+    if(a>b){
+        cout<<"a is greater";
+    }
+    else{
+        cout<<"b is greater";
+    }
+    return 0;
+}
