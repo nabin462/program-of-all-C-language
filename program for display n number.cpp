f3fe7ce13f413ecfd88cdef11a49260fd9d3ed9c
nabin <budhathokinabin700@gmail.com>
2026-07-14 17:46:09 +0545

    add program for display n number

---
+++ program for display n number.cpp
@@ -0,0 +1,13 @@
+//Program to display n number
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        cout<<i<<endl;
+    }
+    return 0;
+    
+}
