3038092255fc0c805362ff3fae2a1df1d61bc15d
nabin <budhathokinabin700@gmail.com>
2026-07-16 18:22:29 +0545

    add area of circle

---
+++ area of circle.cpp
@@ -0,0 +1,13 @@
+//Program to find the area of circle.
+#include<iostream>
+using namespace std;
+int main(){
+    int r,a,pi=3.14;
+    cout<<"enter the radius of circle"<<endl;
+    cin>>r;
+    a=pi*r*r;
+    cout<<"Area="<<a;
+    return 0;
+
+
+}
