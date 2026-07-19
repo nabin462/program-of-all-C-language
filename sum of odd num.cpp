090e97cc3f02993876777b1a0e40c43731f2ae88
nabin <budhathokinabin700@gmail.com>
2026-07-19 16:33:58 +0545

    add sum of odd num

---
+++ sum of odd num.cpp
@@ -0,0 +1,14 @@
+//Program to find the sum of odd number.
+#include<iostream>
+using namespace std;
+int main(){
+    int i, n,sum=0;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        if(i%2!=0)
+        sum=sum+i;
+    }
+    cout<<"sum="<<sum;
+    return 0;
+}
