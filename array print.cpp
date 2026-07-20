a91769267c160e90aaa43e7001d3deb11af23edf
nabin <budhathokinabin700@gmail.com>
2026-07-20 18:04:36 +0545

    add array

---
+++ array print.cpp
@@ -0,0 +1,11 @@
+//Program to print array 
+#include<iostream>
+using namespace std;
+int main(){
+    int i,a[5]={5,6,9,8,6};
+    cout<<"the array element "<<endl;
+    for(i=0;i<5;i++){
+        cout<<a[i]<<endl;
+    }
+    return 0;
+}
