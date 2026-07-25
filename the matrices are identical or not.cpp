48293d4ceaee03c4120d1b51b29067f336eb4efd
nabin <budhathokinabin700@gmail.com>
2026-07-25 18:58:46 +0545

    add the matrices are identical or not

---
+++ the matrices are identical or not.cpp
@@ -0,0 +1,35 @@
+//Program  to check the two matrix are identical or not
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,a[2][2],b[2][2];
+    cout<<"enter the first matrx"<<endl;
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+
+        
+        cin>>a[i][j];
+        }
+    }
+    cout<<"enter the second matrix"<<endl;
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+
+        
+        cin>>b[i][j];
+        }
+    }
+    //for(i=0;i<2;i++){
+        //for(j=0;j<2;j++){
+            if(a[i][j]==b[i][j])
+            {
+                cout<<"the two matrix are identical"<<endl;
+            }
+            else{
+                cout<<" the two matrix are not identical";
+            }
+      //  }
+    //}
+
+    return 0;
+}
