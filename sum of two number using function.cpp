0205e21a2a8b3c920f223b5ec351bb1a130bb23a
nabin <budhathokinabin700@gmail.com>
2026-07-25 18:26:41 +0545

    add sum of two num using function

---
+++ sum of two number using function.cpp
@@ -0,0 +1,16 @@
+//Program to find the sum of two number using function.
+#include<iostream>
+using namespace std;
+void sum(){
+    int a,b,s;
+    cout<<"enter two number"<<endl;
+    cin>>a>>b;
+    s=a+b;
+    cout<<"sum="<<s<<endl;
+
+
+}
+int main(){
+    sum();
+    return 0;
+}
