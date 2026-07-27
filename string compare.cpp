00f67d0ecfbf848126488a91a82a2870088ab232
nabin <budhathokinabin700@gmail.com>
2026-07-27 17:28:51 +0545

    add string compare

---
+++ string compare.cpp
@@ -0,0 +1,13 @@
+//Program to compare the two strng
+#include<iostream>
+using namespace std;
+int main(){
+    string s1="nabin",s2="budhathoki";
+    if(s1.compare(s2)==0){
+        cout<<"string are equal";
+    }
+    else{
+        cout<<"string are not equal";
+    }
+    return 0;
+}
