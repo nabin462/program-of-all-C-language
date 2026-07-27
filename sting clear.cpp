96ab68151d9e611358d6a35f711c4cd1e7364811
nabin <budhathokinabin700@gmail.com>
2026-07-27 17:19:53 +0545

    add string clear

---
+++ sting clear.cpp
@@ -0,0 +1,11 @@
+//Program to clear the string.
+#include<iostream>
+using namespace std;
+int main(){
+    string str="math";
+    str.clear();
+    cout<<"string="<<str<<endl;
+
+  
+    return 0;
+}
