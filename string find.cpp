b46cc84e6966e06dfeecf277de321364ef6bdae1
nabin <budhathokinabin700@gmail.com>
2026-07-28 16:48:59 +0545

    add string finding

---
+++ string find.cpp
@@ -0,0 +1,9 @@
+//programt to find the character in string
+#include<iostream>
+using namespace std;
+int main(){
+    string s="science";
+  
+    cout<<"finding of e="<<  s.find("e")<<endl;
+    return 0;
+}
