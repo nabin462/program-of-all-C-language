d260e66a69d4e78b00c2d7d75c728b63904638b8
nabin <budhathokinabin700@gmail.com>
2026-07-28 16:53:12 +0545

    add string character using index

---
+++ string character acces using index .cpp
@@ -0,0 +1,8 @@
+//Program to access the character from string
+#include<iostream>
+using namespace std;
+int main(){
+    string str="apple";
+    cout<<"the character in str[2]="<<str[2]<<endl;
+    return 0;
+}
