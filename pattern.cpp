3615cb893cb7cee2d14404ef3ec7728b5ede33e1
nabin <budhathokinabin700@gmail.com>
2026-08-23 16:10:46 +0545

    add pattern

---
+++ pattern.cpp
@@ -0,0 +1,13 @@
+//program to print the pattern 
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=5;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
