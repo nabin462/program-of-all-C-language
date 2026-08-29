4bfc592774ecb656d085db4996df1c68e23e27c9
nabin <budhathokinabin700@gmail.com>
2026-08-29 17:27:42 +0545

    add structure question

---
+++ structure question.cpp
@@ -0,0 +1,25 @@
+//program to display the structure question
+#include<iostream>
+using namespace std;
+struct student{
+    char name[20];
+    int id, mark;
+}s[4];
+int main(){
+    int i;
+    for(i=0;i<4;i++){
+        cout<<"enter the name "<<endl;
+        cin>>s[i].name;
+        cout<<"enter id and mark"<<endl;
+        cin>>s[i].id>>s[i].mark;
+    }
+    cout<<"student details"<<endl;
+    for(i=0;i<4;i++){
+        if(s[i].mark>=80){
+        cout<<"name\t id\t mark"<<endl;
+        cout<<s[i].name<<"\t"<<s[i].id<<"\t"<<s[i].mark<<endl;
+        }
+    }
+    return 0;
+
+}
