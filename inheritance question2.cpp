d95c06a0035c7ce79e06fd6e4f9751ca12596e8f
nabin <budhathokinabin700@gmail.com>
2026-09-01 15:12:44 +0545

    add student and clerk derived class

--- inheritance question2.cpp
+++ inheritance question2.cpp
@@ -30,9 +30,39 @@ class doctor:public person{
         show();
         cout<<"h_name="<<h_name<<endl<<"did="<<did<<endl;
     }
+
+};
+
+class student:public person{
+    int sid;
+    float CGPA;
+    public:
+    student(int d,float c,int a,float s):person(a,s){
+        sid=d;
+        CGPA=c;
+    }
+    void dis(){
+        show();
+        cout<<"sid="<<sid<<endl<<"CGPA="<<CGPA<<endl;
+    }
+};
+
+class clerk:public person{
+    public:
+    clerk(int a, float s):person(a,s){
+
+    }
+    void rep(){
+        show();
+    }
 };
 int main(){
     doctor d("nabin",12,21,5200.0);
     d.display();
+    student g(12,3.25,20,2500.0);
+    g.dis();
+     clerk f(20,3000.20);
+     f.rep();
+
     return 0;
 }
