bd4d7a8839853d18f1f9e36b099070c457db6ae7
nabin <budhathokinabin700@gmail.com>
2026-09-03 15:54:18 +0545

    add convert dollor to rupees

---
+++ convert dollor in rupees.cpp
@@ -0,0 +1,12 @@
+//program to display the convert dollor in rupees
+#include<iostream>
+using namespace std;
+int main(){
+     float rupees, dollor;
+    cout<<"enter the dollor to convert into nepali rupees"<<endl;
+    cin>>dollor;
+    rupees=167.5*dollor;
+    cout<<"Rupees="<<rupees<<endl;
+    return 0;
+
+}
